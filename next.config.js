041d6cb8b042cc7f8d5d6eff323b7d1709946f71
unknown <artiomonilov2005@gmail.com>
2022-04-15 18:28:27 +0300

    Context Bug

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    webpack: (config, options) =>
+    {
+        config.module.rules.push({
+            test: /\.pdf$/i,
+            type: 'asset/source'
+        })
+        return config
+    }, devIndicators: {
+        buildActivity: false
+    }
+}
