aafce6b7da208659852e371b68e473c683086bfe
unknown <artiomonilov2005@gmail.com>
2022-05-13 21:03:18 +0300

    Styled Global

---
+++ assets/styles/_BackLink.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+    Link: {
+        '&:hover': {
+            cursor: "pointer",
+        },
+        color: theme.palette.tertiary.main
+    }
+}));
+
+export default useStyles;
