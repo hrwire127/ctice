a552fa6591fe1ac232e0aee70cdea2973b6190e3
unknown <artiomonilov2005@gmail.com>
2022-05-18 19:48:18 +0300

    Profile img upload

---
+++ components/UploadBtnPdf.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Button, Box, IconButton } from "@mui/material";
+import { Clear } from "@mui/icons-material";
+import { uploadFile } from "../utilsCS/_client";
+import useStyles from "../assets/styles/_UploadBtn"
+
+function UploadBtnPdf(props)
+{
+    const { changeFile, file } = props;
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.Upload}>
+            <Button
+                variant="outlined"
+                className={classes.UploadBtn}
+                component="label"
+            >
+                {file
+                    ? file.name
+                    : "Upload Pdf"}
+                <input
+                    type="file"
+                    id="file"
+                    name="file"
+                    onChange={(e) => uploadFile(e, changeFile)}
+                    hidden
+                    accept="application/pdf"
+                />
+            </Button>
+
+            <IconButton
+                onClick={() => changeFile()}
+            >
+                <Clear color="tertiary" />
+            </IconButton>
+        </Box>
+    )
+}
+
+export default UploadBtnPdf
---
+++ components/UploadBtnProfile.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Button, Box, IconButton } from "@mui/material";
+import { Clear } from "@mui/icons-material";
+import { uploadFile } from "../utilsCS/_client";
+import useStyles from "../assets/styles/_UploadBtn"
+
+function UploadBtnProfile(props)
+{
+    const { changeFile, file } = props;
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.Upload}>
+            <Button
+                variant="outlined"
+                className={classes.UploadBtn}
+                component="label"
+            >
+                {file
+                    ? file.name
+                    : "Upload Avatar"}
+                <input
+                    type="file"
+                    id="profile"
+                    name="profile"
+                    onChange={(e) => uploadFile(e, changeFile)}
+                    hidden
+                    accept="image/png, image/jpg, image/jpeg"
+                />
+            </Button>
+
+            <IconButton
+                onClick={() => changeFile()}
+            >
+                <Clear color="tertiary" />
+            </IconButton>
+        </Box>
+    )
+}
+
+export default UploadBtnProfile
