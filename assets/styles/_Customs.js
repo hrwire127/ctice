7e6897851c021a75653cc94cce00e60a5e0b03a8
unknown <artiomonilov2005@gmail.com>
2022-06-24 19:50:26 +0300

    Customs Adapt + new styles

---
+++ assets/styles/_Customs.js
@@ -0,0 +1,29 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+    Body: {
+        flexGrow: 1,
+        padding: "24px"
+    },
+    Container: {
+        display: "flex",
+        justifyContent: "space-evenly",
+        ["@media (max-width:600px)"]: {
+            flexDirection: "column",
+            justifyContent: "center",
+            gap: 10,
+            alignItems: "center",
+        },
+    },
+    Heading: {
+        marginBottom: 4,
+        ["@media (max-width:600px)"]: {
+            textAlign: "center",
+            marginBottom: 8,
+            fontSize: 40
+        },
+    }
+});
+
+
+export default useStyles;
--- components/Customs.js
+++ components/Customs.js
@@ -4,17 +4,37 @@ import { ToggleButton, ToggleButtonGroup, Typography, Box } from '@mui/material'
 import { sortScore, sortDate } from './context/sortEnum'
 import StyleContext from './context/contextStyle'
 import SortContext from './context/contextSort'
+import useStyles from '../assets/styles/_Customs'
 import Redirects_CS from '../utilsCS/CS_Redirects'
+import theme_black from "../assets/images/theme-2.png"
+import theme_white from "../assets/images/theme-1.png"
+import compact_black from "../assets/images/compact-black.png"
+import compact_white from "../assets/images/compact-white.png"
+import full_black from "../assets/images/full-black.png"
+import full_white from "../assets/images/full-white.png"
+import score_black from "../assets/images/score-black.png"
+import score_white from "../assets/images/score-white.png"
+import date_black from "../assets/images/date-black.png"
+import date_white from "../assets/images/date-white.png"
+import useWindowSize from './hooks/useWindowSize'
 
 function Customs(props)
 {
     const { user, setThemeLight, light, setSortCtx, setStyleCtx, setError } = props;
     const sortCtx = React.useContext(SortContext);
     const styleCtx = React.useContext(StyleContext);
-    
+
     const [theme, setTheme] = useState(light ? 'light' : 'dark');
     const [style, setStyle] = useState(styleCtx);
     const [sort, setSorting] = useState(sortCtx);
+    const [windowSize] = useWindowSize();
+
+    const classes = useStyles()
+
+    const imgWidth = windowSize < 400 ? "80%" : (windowSize < 600 ? "60%" : "35%")
+
+    const ImgSelected = { width: imgWidth, opacity: 0.5 }
+    const ImgEmpty = { width: imgWidth, boxShadow: "0 0 10px -5px" }
 
     const handleTheme = (e, newTheme) =>
     {
@@ -27,7 +47,7 @@ function Customs(props)
                         'Content-Type': 'application/json',
                     },
                     body: JSON.stringify(
-                        { light, secret: process.env.NEXT_PUBLIC_SECRET }
+                        { light: newTheme === "light" ? false : true, secret: process.env.NEXT_PUBLIC_SECRET }
                     )
                 }).then(response => response.json())
                 .then(async res =>
@@ -87,56 +107,77 @@ function Customs(props)
 
 
     return (
-        <Box>
-            <Box>
-                <Typography>Theme</Typography>
-                <ToggleButtonGroup
+        <Box
+            component="main"
+            className={classes.Body}
+        >
+            <Box sx={{ p: 2 }}>
+                <Typography className={classes.Heading} variant="h5">Theme</Typography>
+                <Box
                     value={theme}
                     exclusive
                     onChange={handleTheme}
                     aria-label="Theme"
                 >
-                    <ToggleButton value="light" aria-label="light">
-                        <LightMode />
-                    </ToggleButton>
-                    <ToggleButton value="dark" aria-label="dark">
-                        <DarkMode />
-                    </ToggleButton>
-                </ToggleButtonGroup>
+                    <Box className={classes.Container}>
+                        <img src={theme_white.src}
+                            style={theme === "light"
+                                ? ImgSelected
+                                : ImgEmpty}
+                            onClick={(e) => handleTheme(e, "light")} />
+                        <img src={theme_black.src}
+                            style={theme === "dark"
+                                ? ImgSelected
+                                : ImgEmpty}
+                            onClick={(e) => handleTheme(e, "dark")} />
+                    </Box>
+                </Box>
             </Box>
 
-            <Typography>Style</Typography>
-            <Box>
-                <ToggleButtonGroup
+            <Box sx={{ p: 2 }}>
+                <Typography className={classes.Heading} variant="h5">Style</Typography>
+                <Box
                     value={style}
                     exclusive
                     onChange={handleStyle}
-                    aria-label="Card Style"
+                    aria-label="Style"
                 >
-                    <ToggleButton value="full" aria-label="full">
-                        <Lens />
-                    </ToggleButton>
-                    <ToggleButton value="compact" aria-label="compact">
-                        <TableRows />
-                    </ToggleButton>
-                </ToggleButtonGroup>
+                    <Box className={classes.Container}>
+                        <img src={theme === "light" ? full_white.src : full_black.src}
+                            style={style === "full"
+                                ? ImgSelected
+                                : ImgEmpty}
+                            onClick={(e) => handleStyle(e, "full")} />
+                        <img src={theme === "light" ? compact_white.src : compact_black.src}
+                            style={style === "compact"
+                                ? ImgSelected
+                                : ImgEmpty}
+                            onClick={(e) => handleStyle(e, "compact")} />
+                    </Box>
+                </Box>
             </Box>
 
-            <Typography>Sort</Typography>
-            <Box>
-                <ToggleButtonGroup
+            <Box sx={{ p: 2 }}>
+                <Typography className={classes.Heading} variant="h5">Sort</Typography>
+                <Box
                     value={sort}
                     exclusive
-                    onChange={handleSort}
-                    aria-label="Sorting"
+                    onChange={handleStyle}
+                    aria-label="Sort"
                 >
-                    <ToggleButton value={sortDate} aria-label={sortDate}>
-                        <DateRange />
-                    </ToggleButton>
-                    <ToggleButton value={sortScore} aria-label={sortScore}>
-                        <Score />
-                    </ToggleButton>
-                </ToggleButtonGroup>
+                    <Box className={classes.Container}>
+                        <img src={theme === "light" ? date_white.src : date_black.src}
+                            style={sort === "date"
+                                ? ImgSelected
+                                : ImgEmpty}
+                            onClick={(e) => handleSort(e, "date")} />
+                        <img src={theme === "light" ? score_white.src : score_black.src}
+                            style={sort === "score"
+                                ? ImgSelected
+                                : ImgEmpty}
+                            onClick={(e) => handleSort(e, "score")} />
+                    </Box>
+                </Box>
             </Box>
         </Box>
     )
